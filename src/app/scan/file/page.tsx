78c55ae9a4960141116c0a6eacc1230475175b57
JarrarHaider26 <witcoin46@gmail.com>
2026-02-01 19:36:36 +0700

    Update page.tsx

--- src/app/scan/file/page.tsx
+++ src/app/scan/file/page.tsx
@@ -355,19 +355,6 @@ export default function FileScanPage() {
                         </DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
-                <Button 
-                  onClick={() => { 
-                    setScanStatus('idle'); 
-                    setUploadedFile(null); 
-                    setScanResult(null); 
-                    setErrorMessage(null); 
-                  }} 
-                  variant="outline" 
-                  className="w-full sm:w-auto text-sm sm:text-base"
-                >
-                  <ShieldCheck className="mr-2 h-4 w-4" />
-                  Scan Another File
-                </Button>
               </div>
             </CardContent>
           </Card>
